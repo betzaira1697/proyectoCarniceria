2b1cf8dfaea35b48ac59024619877beda84e6c24
ing.Gustavo <aez-castanon@hotmail.com>
2019-01-22 21:06:08 -0600

    conexion
    se agrego la conexion del servidor base de datos no es necesario cambiarla

--- conexion.php
+++ conexion.php
@@ -1,8 +1,8 @@
 <?php
-  $host="localhost";
-  $database="masterpoint";
-  $user="root";
-  $psw="betza123";
+  $host="160.153.43.65";
+  $database="modulo3";
+  $user="hector";
+  $psw="N3VERm0re2018@";
   $db=new mysqli($host,$user,$psw,$database);
   if($db->connect_error>0)
      die("Error de conexion [".$db->connect_error."]");
